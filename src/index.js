c76b8b47dee1a73fa84294d65fd7dc6943908a4a
charuzain <charuabhizain@gmail.com>
2021-12-06 11:55:47 -0500

    css added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
+// step 1: import react from react dependency
+// since it is a dependency in node modue 
+// we use 'react' , if it was a file './filename'
+// Step 2 : Create component 
+// step 3 :Return JSX from component
+// step 4 :since it is a entry point write addidiotnal code
+// where we enject JS Into root of index.html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList(){
+  return (
+    <section className ="bookList">
+    <Book/>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}; 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Book = function(){
+  return(
+    <article className="book">
+      <BookCover />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+const BookCover = function(){ 
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SX338_BO1,204,203,200_.jpg" alt-text="Book Image"/>
+
+  )
+}
+const Title = function () {
+  return (
+    <h3>Diary of a Wimpy Kid</h3>
+  )
+}
+const Author = function(){
+  return (
+    <h4 >Jeff Kinney</h4>
+  );
+}
+ 
+// const BookList = ()=>{
+//   return React.createElement('h1',{},'hi');
+// }
+
+ReactDOM.render(<BookList/>, document.getElementById('root'));
+
